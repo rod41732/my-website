341321e935ae9085bcaa575671d8c8f5811c13ed
Rodchananat K <rod8711@gmail.com>
2019-06-13 14:07:45 +0700

    Make navbar responsive

--- src/layout/navbar.js
+++ src/layout/navbar.js
@@ -4,8 +4,16 @@ import './navbar.css'
 export default function Navbar() {
   return (
     <div>
-      <div className='nav-bar clear-fix'>
+      <div className='nav-bar clear-fix' id="navbar">
         <div className="nav-item logo">Rod41732</div>
+        <div className="nav-item nav-button" onClick={() => {
+          let navClass = document.querySelector("#navbar").classList;
+          if (navClass.contains("expanded")) {
+            navClass.remove("expanded");
+          } else {
+            navClass.add("expanded");
+          }
+        }}> = </div>
         <div className="nav-item">Etc</div>
         <div className="nav-item">Home</div>
         <div className="nav-item">Math</div>
