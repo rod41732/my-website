47167f6820e4a7cc6f765c78bfe78baf9193b4ba
Rodchananat K <rod8711@gmail.com>
2019-06-15 00:35:51 +0700

    Add name and links to cover

--- src/routes/about/components/cover.js
+++ src/routes/about/components/cover.js
@@ -4,8 +4,19 @@ import './cover.css'
 export default function Cover() {
     return (
         <div className="cover clearfix">
-            <img className="cover-img" src="image.jpg"/>
+            <img className="cover-img" style={{
+                backgroundImage: `url(${"/image.jpg"})`
+            }}/>
             <img className="avatar-img" src="doge.png"/>
+            <div className="profile">
+                <div className="profile-line name">Rodchananat K.</div>
+                <div className="profile-line">Labore sunt eiusmod nisi duis.</div>
+            </div>
+            <div className="profile-links">
+                <span className="post-tag">link 1</span>
+                <span className="post-tag">link 2</span>
+                <span className="post-tag">link 3</span>
+            </div>
         </div>
     )
 }
