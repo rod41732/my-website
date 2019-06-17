1e06a5daa0a1cfe4513f8f193ab66231cdcdeddb
Rodchananat K <rod8711@gmail.com>
2019-06-17 23:08:04 +0700

    add Data Query (Following tutorial-4)

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './header.css';
+import {useStaticQuery, graphql} from 'gatsby';
 
 export default function Header({catergoryName}) {
+    const data = useStaticQuery(
+        graphql`
+            query {
+                site {
+                    siteMetadata {
+                        title
+                    }
+                }
+            }
+        `
+    );
     return (
         <div className="header title-font">
-            <h1 className="title">Blog : {catergoryName || 'All'}</h1>
+            <h1 className="title"> {data.site.siteMetadata.title} : {catergoryName || 'All'}</h1>
             <div className="sub-title"> many blog such story much wow lorem ipsum</div>
         </div>
     )
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import Navbar from './navbar';
 import {Helmet} from 'react-helmet';
+import {useStaticQuery, Link, grpahql, graphql} from 'gatsby';
 export default function Layout({children}) {
+    const data = useStaticQuery(
+        graphql`
+            query {
+                site {
+                    siteMetadata {
+                        title
+                    }
+                }
+            }
+        `
+    )
     return (
         <>
             <Helmet>
+                <title>{ data.site.siteMetadata.title }</title>
                 <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
                 <link href={"https://fonts.googleapis.com/css?family=Kanit:400,500&display=swap"} rel="stylesheet"/>
             </Helmet>
