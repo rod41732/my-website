34667e65ce72bd8d9964e017b2df883a264e9538
Rodchananat K <rod8711@gmail.com>
2019-06-23 17:18:26 +0700

    feat: responsive post layout.

--- src/components/post.js
+++ src/components/post.js
@@ -5,31 +5,31 @@ import {Link} from 'gatsby';
 
 export default ({tags, title, text, image, date, link}) => {
     return (
-        <div className="feed-post">
-            <Link to={link}>
-                <div className="image-container clearfix">
+        <div className="feed-post  clearfix">
+            <div className="image-container">
+                <Link to={link}>
                     <img alt={title} className="post-img" src={image || 'image.jpg'}/>
-                    <div className="post-info">
-                        {
-                            // TODO: format date
-                            <div className="post-date secondary-font">{date}</div>
-                        }
-                            <div className="tag-container secondary-font">
-                                {
-                                    tags.map((tagName)  => 
-                                        <Link to={`/list/tag/${tagName}`}><span key={tagName} className="post-tag">{tagName}</span></Link>
-                                    )
-                                }
-                            </div> 
-                        <div className="post-title title-font">{title}</div>
-                    </div>
-                </div>
-            </Link>
+                </Link>
+            </div>
+            <div className="post-info">
+                <div className="post-title title-font">{title}</div>
+                {
+                    // TODO: format date
+                    <div className="post-date secondary-font">{date}</div>
+                }
+                <div className="tag-container secondary-font">
+                    {
+                        tags.map((tagName)  => 
+                            <Link to={`/list/tag/${tagName}`}><span key={tagName} className="post-tag">{tagName}</span></Link>
+                        )
+                    }
+                </div> 
+            </div>
             <p className="post-text">
                 {text || `Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                 Ducimus quas, laboriosam beatae odio expedita itaque ab commodi
-                  voluptatem suscipit officiis, accusantium quia minus numquam!
-                   Dicta alias eligendi eos id doloremque.`
+                    Ducimus quas, laboriosam beatae odio expedita itaque ab commodi
+                    voluptatem suscipit officiis, accusantium quia minus numquam!
+                    Dicta alias eligendi eos id doloremque.`
                 }
             </p>
         </div>
