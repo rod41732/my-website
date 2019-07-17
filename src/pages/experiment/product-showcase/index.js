257c709f9c037e11c408e9263614633de8136ee7
Rodchananat K <rod8711@gmail.com>
2019-07-17 16:53:57 +0700

    add: reveal transition

--- src/pages/experiment/product-showcase/index.js
+++ src/pages/experiment/product-showcase/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 
 import "./style.css"
-import ScrollMagic from "./lib/ScrollMagic"
+import ScrollMagic from "scrollmagic"
 class ShowcaseApp extends React.Component {
   constructor(props) {
     super(props)
@@ -10,49 +10,28 @@ class ShowcaseApp extends React.Component {
   }
 
   componentDidMount() {
-    // new ScrollMagic.Scene({
-    //   triggerElement: "#slide1-product",
-    //   triggerHook: 0.5,
-    //   duration: 0,
-    //   // offset: 50,
-    //   // reverse: false,
-    //   // offset: 50 // move trigger to center of element
-    // })
-    //   .setClassToggle("#slide1-product", "visible") // add class to reveal
-    //   .addIndicators() // add indicators (requires plugin)
-    //   .addTo(this.controller)
+    const contentBoxes = document.querySelectorAll(".content")
 
-    // new ScrollMagic.Scene({
-    //   triggerElement: "#product-wrapper",
-    //   triggerHook: 0.5,
-    //   duration: 0,
-    //   // reverse: false,
-    //   // offset: 50 // move trigger to center of element
-    // })
-    //   .setPin("#slide1-product")
-    //   .addIndicators() // add indicators (requires plugin)
-    //   .addTo(this.controller)
-
-    // new ScrollMagic.Scene({
-    //   triggerElement: "#text-box-1",
-    //   triggerHook: 0.5,
-    //   duration: 0,
-    //   // reverse: false,
-    //   // offset: 50 // move trigger to center of element
-    // })
-    //   .setPin("#text-box-1")
-    //   .addIndicators() // add indicators (requires plugin)
-    //   .addTo(this.controller)
-
-    document.querySelectorAll(".scene").forEach(elem => {
+    contentBoxes.forEach((elem, idx) => {
       new ScrollMagic.Scene({
-        triggerElement: elem,
-        triggerHook: 0.5,
+        triggerElement: elem.parentNode,
+        triggerHook: 0,
         duration: "100%",
       })
-        .setPin(elem)
-        .addIndicators()
+        .setPin(elem.parentNode, {
+          spacerClass: "sm-spacer", // set spacer class for easy reference
+        })
+        .addIndicators({ name: `pin-box ${idx}` })
         .addTo(this.controller)
+
+      new ScrollMagic.Scene({
+        triggerElement: elem.parentNode,
+        triggerHook: 0.25,
+        duration: 0,
+        reverse: false,
+      }).setClassToggle(elem, "visible")
+      .addIndicators(`text-reveal ${idx}`)
+      .addTo(this.controller)
     })
   }
 
@@ -65,13 +44,36 @@ class ShowcaseApp extends React.Component {
         }}
       >
         <div className="wrapper clearfix">
+          <div
+            className="presenter"
+            style={{
+              // placeholder
+              height: "100vh",
+            }}
+          >
+            <h1 className="title-font"> Lorem ipsum 2019</h1>
+            <img
+              src="/product-showcase/laptop.png"
+              width="500px"
+              height="500px"
+            />
+          </div>
           {[1, 2, 3, 4, 5].map((idx, _) => {
             console.log(idx)
             return (
-              <div className="scene">
-                <div>
-                  <div className="content">
-                    {idx}. Hello this is item {idx}
+              <div className="text-scene">
+                <div className="content">
+                  <div className="content-text">
+                    <div className="title title-font">
+                      {idx}. Hello this is item {idx}
+                    </div>
+                    <div className="description secondary-font">
+                      Magna tempor id in nisi irure cupidatat. Ad non proident
+                      exercitation dolore tempor veniam aliqua excepteur
+                      cupidatat in culpa eiusmod. Lorem deserunt ullamco qui in
+                      eu ea deserunt eu reprehenderit elit. Est aute non eu
+                      minim id amet deserunt minim irure cillum.
+                    </div>
                   </div>
                 </div>
               </div>
